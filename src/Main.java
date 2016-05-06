1559aab2222adb58356997ce73db2231bebbc749
jean-michel <jean-michel@delorme.fr>
2016-05-06 15:04:40 +0200

    Ask for a new path

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-
+	private static Scanner scan;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		scan = new Scanner(System.in);
+		String myPath=scan.nextLine();
+		Path goodPath = Paths.get(myPath);
+		File myfile = new File(goodPath.toString());
+		if(!myfile.exists()){
+			try {
+				myfile.mkdir();
+			} catch(Exception e){
+				return;
+			}
+		}
+		
 	}
 
 }
